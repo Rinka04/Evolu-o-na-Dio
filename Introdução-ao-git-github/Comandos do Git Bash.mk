4605c5a8b6480b9bafb67765bf224d1d0598457d
Rinka04 <matheushenrique1704@gmail.com>
2022-06-08 15:26:37 -0300

    Colocado a Lista de Codigos no Arquivo

--- Introdução-ao-git-github/Comandos do Git Bash.mk
+++ Introdução-ao-git-github/Comandos do Git Bash.mk
@@ -2,5 +2,59 @@
 
 
 
-Dir = Lista de Diretórios
+Dir, ls = Lista de Diretórios
+
+ls -a = Mostra as Pastas Ocultas
+
+cd ( Diretórios ) = Leva para o Diretório selecionado
+
+cd .. = Voltar para o diretório anterior
+
+Ctrl + L = Limpa o Terminal 
+
+Mkdir = Faz um pasta (Make Directory)
+
+echo = Devolve no terminal o que foi escrito após o comando "echo"
+
+echo > = Cria um arquivo no formato desejado  
+
+rm = Remove um arquivo (Remove)
+
+rmdir = Remove um Diretório (Remove Directory)
+
+tab = Auto Completa no terminal
+
+git init =  Inicia o Repositório 
+
+git add = Tira o repositório de Staged ou Untracked para Track
+
+git add . ou * = Ativa todos os repositórios
+
+git commit = Cria um commit
+
+git commit -m "Mensagem" = Cria uma commit com uma mensagem 
+
+git status = Mostra os Status de como está o git 
+
+mv ./(diretório) = Mover algo 
+
+git remote add origin = Adicionar um Repositório Remoto a Maquina local 
+
+git pull origin "variavel" = Puxa o Repositório do GitHub
+
+git push origin "variavel" = Empurra o Repositório para o GitHub
+
+git clone (url) = Clona um repositório do GitHub
+
+
+
+
+
+
+
+ 
+
+
+
+
 
